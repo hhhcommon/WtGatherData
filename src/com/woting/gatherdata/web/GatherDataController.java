4958dcef4bddec4fd3c234afe7d9c18f4eb200ee
54PirateGit <18179739967@163.com>
2017-04-28 17:07:02 +0800

    数据收集

--- src/com/woting/gatherdata/web/GatherDataController.java
+++ src/com/woting/gatherdata/web/GatherDataController.java
@@ -1,6 +1,7 @@
 package com.woting.gatherdata.web;
 
 import java.sql.Timestamp;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,7 +17,6 @@ import com.spiritdata.framework.util.StringUtils;
 import com.woting.gatherdata.ApiGatherUtils;
 import com.woting.gatherdata.mem.ApiGatherMemory;
 import com.woting.gatherdata.persis.pojo.ApiLogPo;
-import com.woting.gatherdata.persis.pojo.Data;
 
 @Controller
 public class GatherDataController {
@@ -29,28 +29,47 @@ public class GatherDataController {
         //1-获得原始数据
         String jsonParamStr=(m.get("data")==null?null:(String)m.get("data"));
         if (StringUtils.isNullOrEmptyOrSpace(jsonParamStr)) return null;
-        List<Data> l=null;
+        List<String> l=null;
         try {
-            l=(List<Data>)JsonUtils.jsonToObj(jsonParamStr, List.class);
+            l=(List<String>)JsonUtils.jsonToObj(jsonParamStr, List.class);
         } catch(Exception e) {
         }
         if (l==null) return null;
 
         //2解析原始数据
-        for (Data oneData: l) {
+        for (String oneObj: l) {
+            Map<String, Object> oneData=(Map<String, Object>) JsonUtils.jsonToObj(oneObj, HashMap.class);
+            if (oneData==null) return null;
             ApiLogPo alPo=ApiGatherUtils.buildApiLogDataFromRequest(request);
             //从oneData获取数据，并填入alPo
             //=====================
-            alPo.setBeginTime(new Timestamp(Long.valueOf(oneData.getBeginTime())));
+            if (oneData.get("BeginTime")!=null) {
+                alPo.setBeginTime(new Timestamp(Long.valueOf((String) oneData.get("BeginTime"))));
+            }
             alPo.setDealFlag(1);// 处理成功
             alPo.setOwnerType(201);
-            alPo.setOwnerId(oneData.getUserId());//保存用户ID
-            alPo.setApiName(oneData.getApiName());//保存事件类型
-            alPo.setObjId(oneData.getObjId());
-            alPo.setObjType(oneData.getObjType());
-            alPo.setDeviceId(oneData.getIMEI());
-            alPo.setDeviceType(Integer.valueOf(oneData.getDeviceType()));
-            if (oneData.getReqParam()!=null) alPo.setReqParam(oneData.getReqParam().toString());
+            alPo.setMethod("POST");
+            if (oneData.get("UserId")!=null) {
+                alPo.setOwnerId((String)oneData.get("UserId"));//保存用户ID
+            }
+            if (oneData.get("ApiName")!=null) {
+                alPo.setApiName((String)oneData.get("ApiName"));//保存事件类型
+            }
+            if (oneData.get("ObjId")!=null) {
+                alPo.setObjId((String)oneData.get("ObjId"));
+            }
+            if (oneData.get("ObjType")!=null) {
+                alPo.setObjType((String)oneData.get("ObjType"));
+            }
+            if (oneData.get("IMEI")!=null) {
+                alPo.setDeviceId((String)oneData.get("IMEI"));
+            }
+            if (oneData.get("DeviceType")!=null) {
+                alPo.setDeviceType(Integer.valueOf((String)oneData.get("DeviceType")));
+            }
+            if (oneData.get("ReqParam")!=null) {
+                alPo.setReqParam((String)oneData.get("ReqParam"));
+            }
             alPo.setEndTime(new Timestamp(System.currentTimeMillis()));
             try {
                 ApiGatherMemory.getInstance().put2Queue(alPo);
